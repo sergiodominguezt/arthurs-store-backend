2c50925520cb3b830874e934286b064b755fa764
sergdt <sergiodominguez9710@gmail.com>
2024-09-26 12:35:46 -0500

    fixed script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,7 +5,7 @@ echo 'run application_start.sh: ' >> /home/ec2-user/arthurs-store-backend/deploy
 # Verificar si el proceso está en ejecución
 if ! pm2 describe arthurs-store-backend > /dev/null; then
   echo 'Starting arthurs-store-backend' >> /home/ec2-user/arthurs-store-backend/deploy.log
-  pm2 start /path/to/your/app --name arthurs-store-backend >> /home/ec2-user/arthurs-store-backend/deploy.log
+  pm2 start npm --name arthurs-store-backend -- run start:dev >> /home/ec2-user/arthurs-store-backend/deploy.log
 else
   echo 'Restarting arthurs-store-backend' >> /home/ec2-user/arthurs-store-backend/deploy.log
   pm2 restart arthurs-store-backend >> /home/ec2-user/arthurs-store-backend/deploy.log
