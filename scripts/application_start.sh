f7ba35b71aff7beaa88d6a8e54690c691e567a0c
sergdt <sergiodominguez9710@gmail.com>
2024-09-26 12:22:09 -0500

    fixed script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 echo 'run application_start.sh: ' >> /home/ec2-user/arthurs-store-backend/deploy.log
-# nodejs-app is the same name as stored in pm2 process
-echo 'pm2 restart arthurs-store-backend' >> /home/ec2-user/arthurs-store-backend/deploy.log
-sudo pm2 restart arthurs-store-backend >> /home/ec2-user/arthurs-store-backend/deploy.log
+
+# Verificar si el proceso está en ejecución
+if ! pm2 describe arthurs-store-backend > /dev/null; then
+  echo 'Starting arthurs-store-backend' >> /home/ec2-user/arthurs-store-backend/deploy.log
+  pm2 start /path/to/your/app --name arthurs-store-backend >> /home/ec2-user/arthurs-store-backend/deploy.log
+else
+  echo 'Restarting arthurs-store-backend' >> /home/ec2-user/arthurs-store-backend/deploy.log
+  pm2 restart arthurs-store-backend >> /home/ec2-user/arthurs-store-backend/deploy.log
+fi
