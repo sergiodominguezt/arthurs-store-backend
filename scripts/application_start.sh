c155540315889232c691ea89aa0967f6da0b497d
sergdt <sergiodominguez9710@gmail.com>
2024-09-26 12:11:43 -0500

    fixed script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/arthurs-store-backend/deploy.log
 # nodejs-app is the same name as stored in pm2 process
 echo 'pm2 restart arthurs-store-backend' >> /home/ec2-user/arthurs-store-backend/deploy.log
-pm2 restart arthurs-store-backend >> /home/ec2-user/arthurs-store-backend/deploy.log
+sudo pm2 restart arthurs-store-backend >> /home/ec2-user/arthurs-store-backend/deploy.log
